4aac8b853f6332f41a42930050787694c9f268c1
Hong Tran <jessietran@fb.com>
2021-06-01 22:53:28 -0700

    added user page

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -20,8 +20,48 @@ export default class User extends React.Component {
 
     render(){
         return (
-            <div>
-                
+            <div class="user-container">
+                <div class="user-wrapper">
+                    <div class="identity">
+                        <div class="avatar">
+                            H
+                        </div>
+                        <div class="name-address">
+                            <div class="name">
+                                Hong Tran
+                            </div>
+                            <div class="address">
+                                San Jose, CA
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-stories">
+                        <div class="each-story">
+                            <div class="date">
+                                <p style={{margin: 0}}>Jan</p>
+                                <p style={{margin: 0}}>2</p>
+                            </div>
+                        </div>
+                        <div class="details">
+                            <div class="place">
+                                At Yosemite National Park
+                            </div>
+                            <div class="text" >
+                                Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam. Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.
+                            </div>
+                            <div class="buttons">
+                                <div class="like">
+                                    <div class="icon">
+                                        <i class="far fa-heart"></i>
+                                    </div>
+                                    <div class="number" >
+                                        120
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
